6a5ad4836f279559cdb56c048754af3fe7ae7bcd
Pablo Vazquez <pvazqez@vandldigital.com>
2019-08-17 14:59:14 -0400

    xhr staging with promise

--- js/xhrStaging.js
+++ js/xhrStaging.js
@@ -3,27 +3,50 @@
 //monolithic state that drives all data driven components
 //Need to integrate promises with XHR 
 
-let xhr = new XMLHttpRequest();
 
-xhr.open('GET', 'https://api.punkapi.com/v2/beers');
-xhr.responseType = 'json';
-xhr.send();
-xhr.onload = ()=>{
-    if(xhr.status !== 200){
-        console.log(`Error: ${xhr.status}: ${xhr.statusText}`);
-    }else{
-        console.log(xhr.response);
-    }
-};
+// function getData(method, url, dataType){
+//     let xhr = new XMLHttpRequest();
 
-xhr.onprogress = (event)=>{
-    if(event.lengthComputable){
-        console.log(`Received ${event.loaded} of ${event.total} bytes`);
-    }else{
-        console.log(`Received ${event.loaded} bytes`);
-    }
-};
+//     xhr.open(method, url);
+//     xhr.responseType = dataType;
+//     xhr.send();
+//     xhr.onload = ()=>{
+//         if(xhr.status !== 200){
+//             return `Error: ${xhr.status}: ${xhr.statusText}`;
+//         }else{
+//             return xhr.response;
+//         }
+//     };
+    
+//     xhr.onprogress = (event)=>{
+//         if(event.lengthComputable){
+//             return `Received ${event.loaded} of ${event.total} bytes`;
+//         }else{
+//             return `Received ${event.loaded} bytes`;
+//         }
+//     };
+    
+//     xhr.onerror = ()=>{
+//         return 'Request failed';
+//     };
+// }
 
-xhr.onerror = ()=>{
-    console.log('Request failed');
-};
+function getData(method, url, dataType){
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.responseType = dataType;
+        xhr.onload = () => resolve(xhr.response);
+        xhr.onerror = () => reject(xhr.status);
+        xhr.send();
+    });
+}
+
+let data = getData('GET', 'https://api.punkapi.com/v2/beers', 'json')
+    .then((res)=>{
+        console.log(res);
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
+
